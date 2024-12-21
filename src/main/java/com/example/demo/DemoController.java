599bfea48675cf5ee1591265f32f37d035c02f63
DarrenMing1107 <ieoc08961@hotmail.com>
2024-12-21 22:28:30 +0800

    Second commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 賦予DemoController 處理來自客戶端URL，依照這裡設定的Path去處理對應URL的運算
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Darren!";
+    }
+
+    @GetMapping("/model")
+    public ResponseEntity<DemoModel> getModel(@RequestParam int id, @RequestParam String name) {
+        DemoModel model = new DemoModel(id, name);
+        return new ResponseEntity<>(model, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/demo/DemoModel.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+
+public class DemoModel {
+    private int id;
+    private String name;
+
+    public DemoModel(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
